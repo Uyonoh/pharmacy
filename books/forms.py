5fdf695976dc8ba9bdf25143a401dbd6c7a43e75
Uyonoh <turakiuyonoh@gmail.com>
2022-08-05 12:41:17 +0100

    first code cleanup, pull test

--- books/forms.py
+++ books/forms.py
@@ -4,5 +4,4 @@ from .models import BussinessMonth
 class BussinessMonthForm(forms.ModelForm):
 	class Meta:
 		model = BussinessMonth
-		fields = ["opening_cash", "opening_stock"]
-		
+		fields = ["opening_cash", "opening_stock"]	
--- books/models.py
+++ books/models.py
@@ -4,6 +4,7 @@ from drugs.models import Drug
 
 # Create your models here.
 
+# A model to record cash and stock for each month
 class BussinessMonth(models.Model):
 	opening_cash = models.IntegerField()
 	opening_stock = models.IntegerField()
@@ -14,14 +15,19 @@ class BussinessMonth(models.Model):
 	profit = models.IntegerField(default=0)
 
 	def calculate_profit(self):
+		""" Calculate the profit of a bussiness month """
 		cash_profit = self.closing_cash - self.opening_cash
 		stock_profit = self.closing_stock - self.opening_stock
 		total_profit = cash_profit + stock_profit
 		self.profit = total_profit
 		self.save()
-
 		return cash_profit, stock_profit, total_profit
+	
+	def __str__(self):
+		return f"Bussiness month from {self.opening_date} to {self.closing_date}"
 
+
+# A model to record all credit transactions
 class Credit(models.Model):
 	item = models.CharField(max_length=200)
 	amount = models.IntegerField()
@@ -30,6 +36,8 @@ class Credit(models.Model):
 	def __str__(self) -> str:
 		return f"Credit record of {self.item}"
 
+
+# A model to record all debit transactions
 class Debit(models.Model):
 	item = models.CharField(max_length=200)
 	amount = models.IntegerField()
@@ -38,6 +46,8 @@ class Debit(models.Model):
 	def __str__(self) -> str:
 		return f"Debit record of {self.item}"
 
+
+# A model to record all additions of stock
 class Stock(models.Model):
 	drug = models.ForeignKey(Drug, on_delete=models.CASCADE)
 	price = IntegerField()
