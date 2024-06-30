4636df097e6da5cd22502222e8422179b8517eb0
Uyonoh <turakiuyonoh@gmail.com>
2024-06-30 17:55:11 +0100

    add requirements

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -22,12 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!z4_r*)k-uz182il8xhttz($9%b^(1_1suyravkuk2%gj(slhu'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.43.202']
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME =  os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
